e4450987aa0de311d9bf7d56387a0a43d44108e9
Roberto Ramirez <robert.624@hotmail.com>
2023-04-16 00:48:50 -0500

    working numbers changing according to timeframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,111 @@
 import './App.scss'
 import useStore from './context/store'
+import TimeCard from './components/TimeCard'
+
+const cardsData = [
+  {
+    "title": "Work",
+    "timeframes": {
+      "daily": {
+        "current": 5,
+        "previous": 7
+      },
+      "weekly": {
+        "current": 32,
+        "previous": 36
+      },
+      "monthly": {
+        "current": 103,
+        "previous": 128
+      }
+    }
+  },
+  {
+    "title": "Play",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 2
+      },
+      "weekly": {
+        "current": 10,
+        "previous": 8
+      },
+      "monthly": {
+        "current": 23,
+        "previous": 29
+      }
+    }
+  },
+  {
+    "title": "Study",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 7
+      },
+      "monthly": {
+        "current": 13,
+        "previous": 19
+      }
+    }
+  },
+  {
+    "title": "Exercise",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 5
+      },
+      "monthly": {
+        "current": 11,
+        "previous": 18
+      }
+    }
+  },
+  {
+    "title": "Social",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 3
+      },
+      "weekly": {
+        "current": 5,
+        "previous": 10
+      },
+      "monthly": {
+        "current": 21,
+        "previous": 23
+      }
+    }
+  },
+  {
+    "title": "SelfCare",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 2,
+        "previous": 2
+      },
+      "monthly": {
+        "current": 7,
+        "previous": 11
+      }
+    }
+  }
+]
 
 function App() {
 
@@ -25,6 +131,11 @@ function App() {
           <span id="weekly" onClick={handleChange} className={`${time == 'weekly' ? 'current' : ''}`}>weekly</span>
           <span id="monthly" onClick={handleChange}  className={`${time == 'monthly' ? 'current' : ''}`}>monthly</span>
         </div>
+      </div>
+      <div className="time-cards">
+        {cardsData.map((card) => (
+          <TimeCard card={card}/>
+        ))}
       </div>
     </main>
   )
---
+++ src/components/TimeCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styles from '../styles/TimeCard.module.scss'
+import useStore from '../context/store';
+
+const TimeCard = ({card}) => {
+
+    let {title, timeframes } = card;
+
+    const time = useStore(state => state.time)
+
+    // console.log("time from TimeCard -> ", time)
+
+
+  return (
+    <div className={`${styles.card} ${styles[title]}`}>
+        <div className={styles.card_header}>
+
+        </div>
+        <div className={styles.card_body}>
+            <div className={styles.card_title}>
+                <h2 className={styles.card_title_text}>{title == 'SelfCare' ? 'Self Care': title}</h2>
+                <img className={styles.card_menu} src="icon-ellipsis.svg" alt="card config image" />
+            </div>
+            <div className={styles.current_previous}>
+                <p className={styles.current}>{timeframes[time].current}hrs</p>
+                <span className={styles.previous}>Last Week-{timeframes[time].previous}hrs</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TimeCard
